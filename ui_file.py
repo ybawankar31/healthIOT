c08fb4759ab972772fb8c209925c3885e052b15c
Yash Bawankar <ybawankar31@gmail.com>
2024-05-20 10:58:53 +0530

    just had a play with sample and check file to understand importlib

--- ui_file.py
+++ ui_file.py
@@ -55,10 +55,10 @@ try:
 except Exception as e:
     value_pe = ("Fetching", "Fetching")
 
-try:
-    value_fc = representation_value_cloud.get(timeout= 3)
-except Exception as e:
-    value_fc = ("Fetching", "Fetching")
+# try:
+#     value_fc = representation_value_cloud.get(timeout= 3)
+# except Exception as e:
+#     value_fc = ("Fetching", "Fetching")
 
 try:
     value_fe = representation_value_edge.get(timeout=3)
@@ -86,8 +86,8 @@ class Ui_MainWindow(object):
         self.temp_pe.setText(f"{value_pe[0]}")
         self.time_pe.setText(f"{value_pe[1]}")
         
-        self.temp_fc.setText(f"{value_fc[0]}")
-        self.time_fc.setText(f"{value_fc[1]}")
+        # self.temp_fc.setText(f"{value_fc[0]}")
+        # self.time_fc.setText(f"{value_fc[1]}")
         
         self.temp_fe.setText(f"{value_fe[0]}")
         self.time_fe.setText(f"{value_fe[1]}")
@@ -173,30 +173,19 @@ class Ui_MainWindow(object):
         self.push.setText(_translate("MainWindow", "PUSH"))
         self.fetch.setText(_translate("MainWindow", "FETCH"))
 
-        # self.temp_pc.setText(_translate("MainWindow", f"Fetching"))
-        # self.time_pc.setText(_translate("MainWindow", f"Fetching"))
+        self.temp_pc.setText(_translate("MainWindow", f"Fetching"))
+        self.time_pc.setText(_translate("MainWindow", f"Fetching"))
         
-        # self.temp_pe.setText(_translate("MainWindow", f"Fetching"))
-        # self.time_pe.setText(_translate("MainWindow", f"Fetching"))
+        self.temp_pe.setText(_translate("MainWindow", f"Fetching"))
+        self.time_pe.setText(_translate("MainWindow", f"Fetching"))
         
-        # self.temp_fc.setText(_translate("MainWindow", f"Fetching"))
-        # self.time_fc.setText(_translate("MainWindow", f"Fetching"))
+        self.temp_fc.setText(_translate("MainWindow", f"Fetching"))
+        self.time_fc.setText(_translate("MainWindow", f"Fetching"))
         
-        # self.temp_fe.setText(_translate("MainWindow", f"Fetching"))
-        # self.time_fe.setText(_translate("MainWindow", f"Fetching"))
+        self.temp_fe.setText(_translate("MainWindow", f"Fetching"))
+        self.time_fe.setText(_translate("MainWindow", f"Fetching"))
 
-        self.temp_pc.setText(_translate("MainWindow", f"{value_pc[0]}"))
-        self.time_pc.setText(_translate("MainWindow", f"{value_pc[1]}"))
         
-        self.temp_pe.setText(_translate("MainWindow", f"{value_pe[0]}"))
-        self.time_pe.setText(_translate("MainWindow", f"{value_pe[1]}"))
-        
-        self.temp_fc.setText(_translate("MainWindow", f"{value_fc[0]}"))
-        self.time_fc.setText(_translate("MainWindow", f"{value_fc[1]}"))
-        
-        self.temp_fe.setText(_translate("MainWindow", f"{value_fe[0]}"))
-        self.time_fe.setText(_translate("MainWindow", f"{value_fe[1]}"))
-
 
 
 if __name__ == "__main__":
