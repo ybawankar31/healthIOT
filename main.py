510d66bb323060b18a02256bf89da556b226e573
Yash Bawankar <ybawankar31@gmail.com>
2024-05-20 09:53:14 +0530

    shifted the threads to ui_file

--- main.py
+++ main.py
@@ -25,32 +25,33 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
 
-    #Thread 1 to get data from sensor and put it in queue
-    thread1_sensor_pipeline = threading.Thread(target=generate_sensor_data)
-    thread1_sensor_pipeline.start()
+    # #Thread 1 to get data from sensor and put it in queue
+    # thread1_sensor_pipeline = threading.Thread(target=generate_sensor_data)
+    # thread1_sensor_pipeline.start()
 
+    # obj = MainWin()
+    
+    # #Thread 2 to push the data to cloud
+
+    # thread2_push_to_cloud = threading.Thread(target= cloud_engine)
+    # thread2_push_to_cloud.start()
+
+    # #Thread 3 to push the data to edge ie., local mysql database
+    
+    # thread3_push_to_edge = threading.Thread(target = edge_engine)
+    # thread3_push_to_edge.start()
+
+    # #Thread 4 to pull the data from edge into window. ie., local mysql database
+    
+    # thread4_pull_from_edge = threading.Thread(target = edge_to_window)
+    # thread4_pull_from_edge.start()
+
+    # #Thread 5 to pull the data from cloud into window. ie., free sql database
+    
+    # thread5_pull_from_cloud = threading.Thread(target = cloud_to_window)
+    # thread5_pull_from_cloud.start()
+    
     obj = MainWin()
-    
-    #Thread 2 to push the data to cloud
-
-    thread2_push_to_cloud = threading.Thread(target= cloud_engine)
-    thread2_push_to_cloud.start()
-
-    #Thread 3 to push the data to edge ie., local mysql database
-    
-    thread3_push_to_edge = threading.Thread(target = edge_engine)
-    thread3_push_to_edge.start()
-
-    #Thread 4 to pull the data from edge into window. ie., local mysql database
-    
-    thread4_pull_from_edge = threading.Thread(target = edge_to_window)
-    thread4_pull_from_edge.start()
-
-    #Thread 5 to pull the data from cloud into window. ie., free sql database
-    
-    thread5_pull_from_cloud = threading.Thread(target = cloud_to_window)
-    thread5_pull_from_cloud.start()
-    
 
 
 
